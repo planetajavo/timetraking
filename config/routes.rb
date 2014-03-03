9985f952472df35cf1ed3e91c0ae5a006c04835b
planetajavo <planetajavo@yahoo.es>
2014-03-03 11:27:42 +0000

    contact ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Timetraking::Application.routes.draw do
+
+  get '/' => 'site#home'
+
+  get '/contact' => 'site#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
