042162eb6de7f6b4f36924a171a583c3a63c78c0
planetajavo <planetajavo@yahoo.es>
2014-03-03 10:31:00 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timetraking::Application.config.secret_key_base = '7b99beaea8a3e6f8e845f1a73186dd229cd856e2e0c9adb40aed77e4ffa5a4e3cda3968e7771e22e3882ae3b618b7babb19632c352a92d1ec60cabccaa5d7fb5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Timetraking::Application.config.session_store :cookie_store, key: '_timetraking_session'
